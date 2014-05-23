6a154e92485b4dcb6dae5b2ebab4f3559425638a
pwblaine <pwblaine@gmail.com>
2014-05-23 00:37:55 -0700

    scooted down pictures a little on iPhone 5s

--- buried/LTPhotoDetailViewController.m
+++ buried/LTPhotoDetailViewController.m
@@ -27,6 +27,13 @@
     
     self->imageView.contentMode = UIViewContentModeScaleAspectFill;
     
+    if ((UIDeviceOrientationIsPortrait([[UIApplication sharedApplication] statusBarOrientation])) && (([[UIScreen mainScreen] bounds].size.height-568)?NO:YES))
+    {
+        CGRect statusBarFrame = [[UIApplication sharedApplication] statusBarFrame];
+        
+        self->imageView.frame = CGRectMake(0, statusBarFrame.size.height, self->imageView.frame.size.width, self->imageView.frame.size.height);
+    }
+    
     // set top toolbar to transparent
      [self->topToolbar setBackgroundImage:[[UIImage alloc] init] forToolbarPosition:UIToolbarPositionAny barMetrics:UIBarMetricsDefault];
     
