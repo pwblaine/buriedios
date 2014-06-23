87ff3ed891d2028802d7d0c93e6ccd085020a642
pwblaine <pwblaine@gmail.com>
2014-06-23 10:39:08 -0700

    changed order of operations in the capsule view to fail over gracefully.

--- LTCapsuleViewController.m
+++ LTCapsuleViewController.m
@@ -44,90 +44,102 @@
     self.navigationItem.leftBarButtonItem = backButton;
     
     /*
-    self.navigationItem.rightBarButtonItem = actionButton; */
+     self.navigationItem.rightBarButtonItem = actionButton; */
     
     UIBarButtonItem *trashButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemTrash target:self action:@selector(trashButtonTapped:)];
-     self.navigationItem.rightBarButtonItem = trashButton;
+    self.navigationItem.rightBarButtonItem = trashButton;
     
     self->imageButton.enabled = NO;
     self->thoughtButton.enabled = NO;
     self->thoughtContainer.text = @"";
     self.navigationItem.title = @"Loading...";
+    
 }
 
 -(void)viewWillAppear:(BOOL)animated {
     NSLog(@"<%@:%@:%d>", NSStringFromClass([self class]), NSStringFromSelector(_cmd), __LINE__);
-    self->activityIndicator.alpha = 1;
-    self->theThought = [self.capsule objectForKey:@"thought"];
+    
 }
 
 -(void)viewDidAppear:(BOOL)animated {
     NSLog(@"<%@:%@:%d>", NSStringFromClass([self class]), NSStringFromSelector(_cmd), __LINE__);
+    
     [self->activityIndicator startAnimating];
     
     NSString *fromUserId = [self.capsule objectForKey:@"fromUserId"];
     PFQuery *fromUserIdQuery = [PFUser query];
     [fromUserIdQuery whereKey:@"objectId" equalTo:fromUserId];
     [fromUserIdQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (!error)
-        {
+        if (!error) {
             PFUser *user = [objects firstObject];
             NSString *displayName = [user objectForKey:@"displayName"];
             if (displayName.length > 0)
-                self.title = [NSString stringWithFormat:@"From %@", displayName];
-        }
-    }];
-    
-    self->imageContainer.file = [self.capsule objectForKey:@"image"];
-    
-    [self->imageContainer loadInBackground:^(UIImage *image, NSError *error) {
-        if (error) {
-            self->thoughtContainer.text = @"couldn't find your capsule, please refresh";
-        }
-        else if (image)
-        {
-            
-            // this code is run if a picture is downloaded
-            self->theImage = image; // store image in property
-
-            self->imageContainer.image = image;
-            self->imageContainer.frame = CGRectMake((320-285)/2, 130, 285, 285);
-            self->imageButton.frame = CGRectMake((320-285)/2, 125, 285, 285);
-            
-            self->imageContainer.contentMode = UIViewContentModeScaleAspectFit;
-            
-            self->thoughtContainer.frame = CGRectMake(20, 415, 280, 65);
-            self->thoughtButton.frame = CGRectMake(20, 415, 280, 65);
-            
-            self->imageButton.enabled = YES;
-            
-            LTAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
-            [appDelegate.grassDelegate setGrassState:LTGrassStateShrunk animated:YES];
-            
+            {
+                self.navigationItem.title = [NSString stringWithFormat:@"From %@", displayName];
+            }
         } else {
-            self->imageContainer.image = nil;
-            LTAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
-            [appDelegate.grassDelegate setGrassState:LTGrassStateGrown animated:YES];
+            NSLog(@"error in loading fromUserId query: %@",error);
+            self.navigationItem.title = @"Capsule";
         }
-        // this code runs whether there's an image or not after its been retrieved
-                if (self->theThought.length > 1)
-                self->thoughtButton.enabled = YES;
-                else
-        {
-            LTAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
-            [appDelegate.grassDelegate setGrassState:LTGrassStateGrown animated:YES];
-
-        }
-        if ([self->imageContainer.image isEqual:[UIImage imageWithContentsOfFile:@"burieddot152.png"]])
-            self->imageContainer.image = nil;
-        self->thoughtContainer.text = self->theThought;
-        [self->activityIndicator stopAnimating];
-        [self->activityIndicator setAlpha:0];
-        NSString *timestampString = [NSDateFormatter localizedStringFromDate:self.capsule.createdAt dateStyle:NSDateFormatterMediumStyle timeStyle:NSDateFormatterShortStyle];
-        self->timestamp.text = timestampString; // timestamp states created at date
     }];
     
+    NSString *timestampString = [NSDateFormatter localizedStringFromDate:self.capsule.createdAt dateStyle:NSDateFormatterMediumStyle timeStyle:NSDateFormatterShortStyle];
+    self->timestamp.text = timestampString; // timestamp states created at date
+    // this code runs whether there's an image or not after its been retrieved
+    if (self->theThought.length > 1)
+    {
+        LTAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+        [appDelegate.grassDelegate setGrassState:LTGrassStateGrown animated:YES];
+        self->thoughtButton.enabled = YES;
+        self->thoughtContainer.text = self->theThought;
+    } else {
+        [self->thoughtButton removeFromSuperview];
+        [self->thoughtContainer removeFromSuperview];
+    }
+    
+    PFFile *file = [self.capsule objectForKey:@"image"];
+    
+    if (!file) {
+        NSLog(@"No file detected");
+        [self->imageButton removeFromSuperview];
+        [self->imageContainer removeFromSuperview];
+    } else {
+        
+        [self->imageContainer loadInBackground:^(UIImage *image, NSError *error) {
+            
+            [self->activityIndicator stopAnimating];
+            [self->activityIndicator setAlpha:0];
+            
+            if (!error)
+            {
+                
+                // this code is run if a picture is downloaded
+                self->theImage = image; // store image in property
+                
+                self->imageContainer.image = image;
+                self->imageContainer.frame = CGRectMake((320-285)/2, 130, 285, 285);
+                self->imageButton.frame = CGRectMake((320-285)/2, 125, 285, 285);
+                self->imageContainer.contentMode = UIViewContentModeScaleAspectFit;
+                
+                if (self->theThought.length > 1)
+                {
+                    self->thoughtContainer.frame = CGRectMake(20, 415, 280, 65);
+                    self->thoughtButton.frame = CGRectMake(20, 415, 280, 65);
+                }
+                
+                self->imageButton.enabled = true;
+                
+                LTAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+                [appDelegate.grassDelegate setGrassState:LTGrassStateShrunk animated:YES];
+            } else {
+                NSLog(@"image failed to load error: %@",error);
+                [self->imageContainer removeFromSuperview];
+                [self->imageButton removeFromSuperview];
+            }
+        }];
+    }
 }
+
 
 - (void)viewWillDisappear:(BOOL)animated
 {
@@ -171,11 +183,11 @@
     NSLog(@"<%@:%@:%d>", NSStringFromClass([self class]), NSStringFromSelector(_cmd), __LINE__);
     if (self->theThought.length > 1)
     {
-    LTThoughtDetailViewController *thoughtDetailViewController = [[LTThoughtDetailViewController alloc] init];
-    thoughtDetailViewController.theThought = self->theThought;
+        LTThoughtDetailViewController *thoughtDetailViewController = [[LTThoughtDetailViewController alloc] init];
+        thoughtDetailViewController.theThought = self->theThought;
         thoughtDetailViewController.callingViewController = self;
-    thoughtDetailViewController.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
-    [self presentViewController:thoughtDetailViewController animated:YES completion:nil];
+        thoughtDetailViewController.modalTransitionStyle = UIModalTransitionStyleCoverVertical;
+        [self presentViewController:thoughtDetailViewController animated:YES completion:nil];
     }
 }
 
@@ -188,7 +200,7 @@
 {
     NSLog(@"<%@:%@:%d", NSStringFromClass([self class]), NSStringFromSelector(_cmd), __LINE__);
     LTBuryItViewController *buryItViewController = [[LTBuryItViewController alloc] init];
-
+    
     buryItViewController.capsuleImage = self->theImage;
     buryItViewController.capsuleThought = self->theThought;
     [self.navigationController pushViewController:buryItViewController animated:YES];
@@ -220,16 +232,16 @@
                 NSLog(@"capsule left in database with no users, popping to unearthed view");
                 
                 /*NSLog(@"capsule identified as empty, removing from db...");
-                [self.capsule deleteInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-                    if (succeeded)
-                    {
-                        NSLog(@"capsule %@ has been successfully removed from the database, popping to unearthed view",capsuleId);
-                        [self.navigationController popViewControllerAnimated:YES];
-                    } else {
-                        NSLog(@"deleting capsule %@ failed with error %@, please try again",error,capsuleId);
-                    }
-                    
-                }];*/
+                 [self.capsule deleteInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                 if (succeeded)
+                 {
+                 NSLog(@"capsule %@ has been successfully removed from the database, popping to unearthed view",capsuleId);
+                 [self.navigationController popViewControllerAnimated:YES];
+                 } else {
+                 NSLog(@"deleting capsule %@ failed with error %@, please try again",error,capsuleId);
+                 }
+                 
+                 }];*/
             } else if ([[self.capsule objectForKey:@"toUserIds"] count] != 0)
             {
                 NSLog(@"toUserIds for capsule %@: %@, popping to unearthed view...",capsuleId,toUsersArray);
